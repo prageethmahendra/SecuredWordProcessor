f6d152470ad6826bd7cb2ffd796887d960620b97
prageethmahendra <sithumina>
2016-02-01 21:34:04 +0800

    security features added

--- src/main/java/word/counter/RestServiceApplication.java
+++ src/main/java/word/counter/RestServiceApplication.java
@@ -3,17 +3,31 @@ package word.counter;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
+import word.counter.web.resources.WordCounter;
 
+import javax.ws.rs.core.Application;
 import java.io.IOException;
 import java.net.URI;
+import java.util.HashSet;
+import java.util.Set;
 
 
 /**
  * This is the Rest Service Application class which will initiate the service using a main method
  * NOTE : no need to deploy to a web server as this is a POC
  */
-public class RestServiceApplication {
+public class RestServiceApplication  extends Application {
     public static final String BASE_URI = "http://localhost:8080/wordcounter/";
+
+    public RestServiceApplication() {
+    }
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> s = new HashSet<Class<?>>();
+        s.add(WordCounter.class);
+        return s;
+    }
 
     /**
      * Server start method
@@ -24,12 +38,12 @@ public class RestServiceApplication {
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
     }
 
-    public static void main(String[] args) throws IOException {
-        final HttpServer server = startServer();
-        System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-        System.in.read();
-        server.stop();
-    }
+//    public static void main(String[] args) throws IOException {
+//        final HttpServer server = startServer();
+//        System.out.println(String.format("Jersey app started with WADL available at "
+//                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+//        System.in.read();
+//        server.stop();
+//    }
 }
 
